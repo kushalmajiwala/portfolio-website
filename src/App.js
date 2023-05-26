f83465ce9e272a993c0832067500d10293aa6cd7
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-05-26 18:30:14 +0530

    primereact added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { useState } from 'react';
+
+
 const App = () => {
-  return(
+  const [visible, setVisible] = useState(false);
+
+  return (
     <>
       <h1>I am Kushal Majiwala...And this is my portfolio Website...</h1>
+      <div>
+            <Button label="Show" icon="pi pi-external-link" onClick={() => setVisible(true)} />
+            <Dialog header="My Info" visible={visible} draggable={false} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+                <p className="m-0">
+                    I am Kushal Majiwala and I am an IT Student
+                </p>
+            </Dialog>
+        </div>
     </>
   )
 }
