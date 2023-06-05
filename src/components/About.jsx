30f3ee670e63602d2c94d8bb354ae67ba99f6e20
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-06-05 21:44:02 +0530

    responsive problme testing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-const About = () => {
+export const About = () => {
 
     const handleIconClick = (iconName) => {
         if (iconName === "instagram") {
@@ -35,16 +35,18 @@ const About = () => {
                 <span className='about-text'>ABOUT</span>
             </div>
             <div className='about-content-container'>
-                <h1 style={{ marginLeft: '-3%', color:'#f77a52' }}>Kushal Majiwala</h1>
-                <div className='about-content'>
-                    <p>My name is Kushal. I live in Surat. I am a full stack developer. I am a passionate learner. I have completed MSCIT from VNSGU. I have completed 6 months internship at Avinashi Ventures. I worked in Vuejs for frontend and .NET for backend during that time period. I worked on two projects in my internship.</p>
-                    <p>I have worked in many frontend projects and deployed those projects on internet using cloudflare web service.</p>
-                    <p>I also have expertise in AWS Lambda. I have also built many projects in ReactJS and I used Supabase as backend for database operations. </p>
-                    <div className='icons-container'>
-                        <p className='about-icon instaIcon' onClick={() => handleIconClick("instagram")}><i class="bi bi-instagram"></i></p>
-                        <p className='about-icon whaIcon' onClick={() => handleIconClick("whatsapp")}><i class="bi bi-whatsapp"></i></p>
-                        <p className='about-icon linkIcon' onClick={() => handleIconClick("linkedin")}><i class="bi bi-linkedin"></i></p>
-                        <p className='about-icon gitIcon' onClick={() => handleIconClick("github")}><i class="bi bi-github"></i></p>
+                <div className='about-inner-container'>
+                    <h1 style={{ marginLeft: '-3%', color: '#f77a52' }}>Kushal Majiwala</h1>
+                    <div className='about-content'>
+                        <p>My name is Kushal. I live in Surat. I am a full stack developer. I am a passionate learner. I have completed MSCIT from VNSGU. I have completed 6 months internship at Avinashi Ventures. I worked in Vuejs for frontend and .NET for backend during that time period. I worked on two projects in my internship.</p>
+                        <p>I have worked in many frontend projects and deployed those projects on internet using cloudflare web service.</p>
+                        <p>I also have expertise in AWS Lambda. I have also built many projects in ReactJS and I used Supabase as backend for database operations. </p>
+                        <div className='icons-container'>
+                            <p className='about-icon instaIcon' onClick={() => handleIconClick("instagram")}><i class="bi bi-instagram"></i></p>
+                            <p className='about-icon whaIcon' onClick={() => handleIconClick("whatsapp")}><i class="bi bi-whatsapp"></i></p>
+                            <p className='about-icon linkIcon' onClick={() => handleIconClick("linkedin")}><i class="bi bi-linkedin"></i></p>
+                            <p className='about-icon gitIcon' onClick={() => handleIconClick("github")}><i class="bi bi-github"></i></p>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,33 +14,35 @@ const Experience = () => {
                     <img src='https://ivabqohtumjadxnipnsa.supabase.co/storage/v1/object/public/images/work_experience.png' alt="no-image" className='developer-image-exp' />
                 </div>
                 <div className='exp-info-container'>
-                    <h1 style={{ color: '#f77a52' }}>FULL STACK DEVELOPER</h1>
-                    <div className='exp-content'>
-                        <p>I have completed 6 months internship at Avinashi Ventures.Where I worked as full stack developer.
-                            I used different technologies like VueJS, ReactJS, Svelte, .Net, NodeJS in that time period.
-                            I completed two project in my internship.
-                        </p>
-                    </div>
-                    <div className='skill-container'>
-                        <p className='skill-name'>ReactJS</p>
-                        <div style={{ width: '90%' }}>
-                            <Tooltip target=".reactSkill" />
-                            <p style={{ display: 'flex' }}><p className='skill-percent reactSkill' data-pr-tooltip="90%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalReactSkill'></p></p>
+                    <div>
+                        <h1 style={{ color: '#f77a52' }}>FULL STACK DEVELOPER</h1>
+                        <div className='exp-content'>
+                            <p>I have completed 6 months internship at Avinashi Ventures.Where I worked as full stack developer.
+                                I used different technologies like VueJS, ReactJS, Svelte, .Net, NodeJS in that time period.
+                                I completed two project in my internship.
+                            </p>
                         </div>
-                        <p cpName='skill-name'>VueJS</p>
-                        <div style={{ width: '90%' }}>
-                            <Tooltip target=".vueSkill" />
-                            <p style={{ display: 'flex' }}><p className='skill-percent vueSkill' data-pr-tooltip="70%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalVueSkill'></p></p>
-                        </div>
-                        <p className='skill-name'>NodeJS</p>
-                        <div style={{ width: '90%' }}>
-                            <Tooltip target=".nodeSkill" />
-                            <p style={{ display: 'flex' }}><p className='skill-percent nodeSkill' data-pr-tooltip="80%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalNodeSkill'></p></p>
-                        </div>
-                        <p className='skill-name'>Java</p>
-                        <div style={{ width: '90%' }}>
-                            <Tooltip target=".javaSkill" />
-                            <p style={{ display: 'flex' }}><p className='skill-percent javaSkill' data-pr-tooltip="60%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalJavaSkill'></p></p>
+                        <div className='skill-container'>
+                            <p className='skill-name'>ReactJS</p>
+                            <div style={{ width: '90%' }}>
+                                <Tooltip target=".reactSkill" />
+                                <p style={{ display: 'flex' }}><p className='skill-percent reactSkill' data-pr-tooltip="90%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalReactSkill'></p></p>
+                            </div>
+                            <p cpName='skill-name'>VueJS</p>
+                            <div style={{ width: '90%' }}>
+                                <Tooltip target=".vueSkill" />
+                                <p style={{ display: 'flex' }}><p className='skill-percent vueSkill' data-pr-tooltip="70%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalVueSkill'></p></p>
+                            </div>
+                            <p className='skill-name'>NodeJS</p>
+                            <div style={{ width: '90%' }}>
+                                <Tooltip target=".nodeSkill" />
+                                <p style={{ display: 'flex' }}><p className='skill-percent nodeSkill' data-pr-tooltip="80%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalNodeSkill'></p></p>
+                            </div>
+                            <p className='skill-name'>Java</p>
+                            <div style={{ width: '90%' }}>
+                                <Tooltip target=".javaSkill" />
+                                <p style={{ display: 'flex' }}><p className='skill-percent javaSkill' data-pr-tooltip="60%" data-pr-position="top" data-pr-at="right top"></p><p className='total-skill-percent totalJavaSkill'></p></p>
+                            </div>
                         </div>
                     </div>
                 </div>
