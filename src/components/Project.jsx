2c255df01895232145fe4131782362c6877f1eff
Kushal Majiwala <kushalmajiwala1212@gmail.com>
2023-06-06 19:59:48 +0530

    portfolio website complete

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,65 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import AllProject from './projectapi';
+import { DataView } from 'primereact/dataview';
+import { Button } from 'primereact/button';
+import { Rating } from 'primereact/rating';
+import { Tooltip } from 'primereact/tooltip';
+
 
 const Project = () => {
+
+    const [projectData, setProjectData] = useState([]);
+
+    useEffect(() => {
+        setProjectData(AllProject);
+    }, [])
+
+    const handleViewButtonClick = (project_url) => {
+        let a = document.createElement('a');
+        a.target = '_blank';
+        a.href = project_url;
+        a.click();
+    }
+
+    const ProjectCard = (project) => {
+        return (
+            <>
+                <div className="project-card-container">
+                    <div className="project-card-inner-container">
+                        <div className='project-pic-container'>
+                            <img className="project-image" src={`${project.image}`} alt={project.name} />
+                        </div>
+                        <div className="project-desc-container">
+                            <div className="project-desc">
+                                <div className='project-name-btn-container'>
+                                    <div className="project-name">{project.name}</div>
+                                    <div className="visit-button-container">
+                                        <Tooltip target=".visit-btn">
+                                            View Project
+                                        </Tooltip>
+                                        <Button icon="pi pi-eye" className="p-button-rounded visit-btn" data-pr-position='bottom' onClick={() => handleViewButtonClick(project.link)}></Button>
+                                    </div>
+                                </div>
+                                <Rating value={project.star} readOnly cancel={false} className="project-rating"></Rating>
+                                <div className="project-desc">
+                                    <span>{project.desc}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <div className='project-heading-container'>
                 <span className='project-text'>PROJECTS</span>
             </div>
             <div className='project-content-container'>
-                
+                <DataView value={projectData} itemTemplate={ProjectCard} style={{ border:'none' }} />
             </div>
         </>
     )
---
+++ src/components/projectapi.js
@@ -0,0 +1,32 @@
+const AllProject = [
+    {
+        image: 'https://ivabqohtumjadxnipnsa.supabase.co/storage/v1/object/public/images/cv.png',
+        name: 'CV Managerment System',
+        star: '4',
+        desc: 'This was one of my college projects. I used .NET, AWS, Svelte in this project.',
+        link: 'https://cvproject.pages.dev/kushal_1212'
+    },
+    {
+        image: 'https://ivabqohtumjadxnipnsa.supabase.co/storage/v1/object/public/images/To-do-List-image.jpg',
+        name: 'To Do List Project',
+        star: '5',
+        desc: 'This was one of my personal projects. I used ReactJS and Supabase in this project.',
+        link: 'https://react-todo-personal-list.pages.dev/'
+    },
+    {
+        image: 'https://ivabqohtumjadxnipnsa.supabase.co/storage/v1/object/public/images/weather.png',
+        name: 'Weather Application',
+        star: '3',
+        desc: 'This was one of my personal projects. I used ReactJS and Weather API in this project.',
+        link: 'https://weather-application-akn.pages.dev/'
+    },
+    {
+        image: 'https://ivabqohtumjadxnipnsa.supabase.co/storage/v1/object/public/images/calc.png',
+        name: 'Calculator Application',
+        star: '4',
+        desc: 'his was one of my personal projects. I used ReactJS in this project.',
+        link: 'https://calculator-application-2ub.pages.dev/'
+    }
+];
+
+export default AllProject;
